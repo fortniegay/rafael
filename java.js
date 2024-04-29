862514d1b40b646f434e353363ffc12beeaf720b
fortniegay <133384076+fortniegay@users.noreply.github.com>
2024-04-29 11:02:24 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,15 @@
+const textos = document.querySelectorAll(".aba-conteudo");
+textos[i].classList.add("ativo");
+textos[j].classList.remove("ativo");
+const contadores = document.querySelectorAll(".contador");
+constcontadores = document.querySelectorAll(".contador");
+contadores[0]
+contadores[0].textContent = "Contagem regressiva";
+constcontadores = document.querySelectorAll(".contador");
+consttempoObjetivo1 = new Date("2024-10-05");
+constcontadores = document.querySelectorAll(".contador");
+consttempoObjetivo1 = new Date("2024-10-05");
+contadores[0].textContent = tempoObjetivo1;
+constcontadores = document.querySelectorAll(“.contador”);
+consttempoObjetivo1 = new Date(“2024-10-05T00:00:00”);
+contadores[0].textContent = tempoObjetivo1;
